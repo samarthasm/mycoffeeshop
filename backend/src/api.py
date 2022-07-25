854aca86a610475e65bf27a9fe8d59f24163dbac
Samartha S M <Samartha.SM@shell.com>
2022-07-25 17:01:12 +0530

    Committing changes to api.py, auth.py and models.py

--- backend/src/api.py
+++ backend/src/api.py
@@ -8,16 +8,43 @@ from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
+# To run Server, executing from backend directory:
+# set FLASK_APP=api.py
+# set FLASK_ENV=development
+# python -m flask run
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this funciton will add one
-'''
-# db_drop_and_create_all()
+#----------------------------------------------------------------------------#
+# TODO_complete
+# Uncommentefd the following line to initialize the database
+#----------------------------------------------------------------------------#
+
+db_drop_and_create_all()
+
+#----------------------------------------------------------------------------#
+# The following custom method queries the list of drinks with a string 
+# input (long/short recipe description) and outputs the list of drinks.
+# If no drinks are found, it aborts with 404 error.
+#----------------------------------------------------------------------------#
+
+def get_all_drinks(recipe_format):
+
+    # Get all drinks in database
+    all = Drink.query.order_by(Drink.id).all()
+    # Format with different recipe detail level
+    if recipe_format.lower() == 'short':
+        all_drinks_formatted = [drink.short() for drink in all]
+    elif recipe_format.lower() == 'long':
+        all_drinks_formatted = [drink.long() for drink in all]
+    else:
+        return abort(500, {'message': 'bad formatted function call. recipe_format needs to be "short" or "long".'})
+
+    if len(all_drinks_formatted) == 0:
+        abort(404, {'message': 'no drinks found in database.'})
+    
+    # Return formatted list of drinks
+    return all_drinks_formatted
 
 # ROUTES
 '''
@@ -75,12 +102,11 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
-
-# Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-
+#----------------------------------------------------------------------------#
+# Error Handlers
+# TODO_complete
+# Implementing error handlers using the @app.errorhandler(error) decorator
+#----------------------------------------------------------------------------#
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -90,25 +116,28 @@ def unprocessable(error):
         "message": "unprocessable"
     }), 422
 
+#----------------------------------------------------------------------------#
+# TODO_complete 
+# Implementing error handler for 404
+#----------------------------------------------------------------------------#
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
+@app.errorhandler(404)
+def bad_request(error):
+    return jsonify({
+                    "success": False, 
                     "error": 404,
                     "message": "resource not found"
                     }), 404
 
-'''
+#----------------------------------------------------------------------------#
+# TODO_complete 
+# Implementing error handler for AuthError
+#----------------------------------------------------------------------------#
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
+@app.errorhandler(AuthError)
+def authentification_failed(AuthError): 
+    return jsonify({
+                    "success": False, 
+                    "error": AuthError.status_code,
+                    "message": "authentification fails"
+                    }), 401
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -9,11 +9,10 @@ database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filenam
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
-
+#----------------------------------------------------------------------------#
+# The following method setup_db(app) binds a flask application and a 
+# SQLAlchemy service
+#----------------------------------------------------------------------------#
 
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -21,32 +20,50 @@ def setup_db(app):
     db.app = app
     db.init_app(app)
 
-
-'''
-db_drop_and_create_all()
-    drops the database tables and starts fresh
-    can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
-'''
-
+#----------------------------------------------------------------------------#
+# The following method db_drop_and_create_all() drops the database tables and
+# starts fresh
+#----------------------------------------------------------------------------#
 
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
     # add one demo row which is helping in POSTMAN test
-    drink = Drink(
-        title='water',
-        recipe='[{"name": "water", "color": "blue", "parts": 1}]'
-    )
+    drink_1 = Drink((
+        id=1      
+        title='milky-water',
+        recipe="""[{"name": "water",
+                    "color": "blue", 
+                    "parts": 1
+                    },
+                    {"name": "milk",
+                    "color": "white", 
+                    "parts": 3
+                    }
+                  ]"""
+                    ))
+    drink_2 = Drink((
+        id=1      
+        title='cherry-lemon',
+        recipe="""[{"name": "cherry",
+                    "color": "red", 
+                    "parts": 2
+                    },
+                    {"name": "lemon",
+                    "color": "yellow", 
+                    "parts": 2
+                    }
+                  ]"""
+                    ))
+
+    drink_1.insert()
+    drink_2.insert()
 
 
-    drink.insert()
-# ROUTES
-
-'''
-Drink
-a persistent drink entity, extends the base SQLAlchemy Model
-'''
+#----------------------------------------------------------------------------#
+# class Drink
+# a persistent drink entity, extends the base SQLAlchemy Model
+#----------------------------------------------------------------------------#
 
 
 class Drink(db.Model):
